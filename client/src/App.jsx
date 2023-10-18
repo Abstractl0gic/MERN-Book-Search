abffef4d605c1d7518ff0837e1a0dc9e58b23224
abstractl0gic <zwlyons@gmail.com>
2023-10-18 15:38:15 -0500

    add graphQL functionality to app,jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,38 @@
-import './App.css';
-import { Outlet } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
-
 import Navbar from './components/Navbar';
+import SearchBooks from './pages/SearchBooks';
+import SavedBooks from './pages/SavedBooks';
 
 // Create an Apollo Client instance
 const client = new ApolloClient({
-  uri: '/graphql', // Set the GraphQL endpoint
+  request: (operation) => {
+    const token = localStorage.getItem("id_token");
+
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : "",
+      },
+    });
+  },
+  uri: "/graphql", // Set the GraphQL endpoint
   cache: new InMemoryCache(),
 });
 
 function App() {
   return (
     <ApolloProvider client={client}>
-      <Navbar />
-      <Outlet />
+      <Router>
+        <>
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={SearchBooks} />
+            <Route exact path='/saved' component={SavedBooks} />
+            <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
+          </Switch>
+        </>
+      </Router>
     </ApolloProvider>
   );
 }
